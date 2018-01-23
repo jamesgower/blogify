d557a4dab64629f6d47464047b4500378302e057
James <james_gower@outlook.com>
2018-01-23 14:49:15 +0000

    removed senesitive firebase info

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-	apiKey: "AIzaSyAaTT2IEvsS0RiT82aUQHtqtA7-MN-olVo",
-	authDomain: "react-16-blog.firebaseapp.com",
-	databaseURL: "https://react-16-blog.firebaseio.com",
-	projectId: "react-16-blog",
-	storageBucket: "react-16-blog.appspot.com",
-	messagingSenderId: "688488618130"
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
